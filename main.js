6c245103ab0a8bc0914b70e6561b8f51082f589b
randall murphy <rmurphy@perseverenow.org>
2025-03-19 14:13:44 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,23 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+const fizzy = (num) => {
+  if()
+}
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+
+if (typeof fizzy === "undefined") {
+  fizzy = undefined;
+}
+
+module.exports = {
+  fizzy,
+};
