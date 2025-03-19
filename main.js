61aa2928b364ef1b3131c5935ee35eef7476f921
randall murphy <rmurphy@perseverenow.org>
2025-03-19 14:26:46 -0500

    final commit

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 const fizzy = (num) => {
-  if()
+  if(num % 3 === 0 && num % 5 === 0){
+    return "FizzBuzz";
+  } else if( num % 3 === 0){
+    return "Fizz";
+  } else if(num % 5 === 0){
+    return "Buzz";
+  } else {
+    return num;
+  }
+
 }
 
 
